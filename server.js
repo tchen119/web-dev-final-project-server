a198539dd531dc8a9384487a7d55bccd1f2ddaa1
Tina C <tinac@Tinas-MacBook-Air.local>
2022-04-13 16:26:30 -0400

    fixed error

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import yelpController from './controllers/yelp-controller.js';
-import businessesController from './controllers/businesses-controller.js';
-import usersController from './controller/users-controller.js';
+//import businessesController from './controllers/businesses-controller.js';
+//import usersController from './controller/users-controller.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 yelpController(app);
-businessesController(app);
-usersController(app);
+//businessesController(app);
+//usersController(app);
 
 app.listen(process.env.PORT || 4000);
