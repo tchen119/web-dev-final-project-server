61010caee6805195f15f28d68a0409f5c8ec7528
Tina C <tinac@Tinas-MacBook-Air.local>
2022-04-18 19:35:28 -0400

    remote mongo server

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import yelpController from './controllers/yelp-controller.js';
 import businessesController from './controllers/businesses-controller.js';
 //import usersController from './controller/users-controller.js';
 
-mongoose.connect('mongodb://localhost:27017/final_project');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/final_project';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
