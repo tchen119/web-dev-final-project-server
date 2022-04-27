de456d5e4ea05a01c7151837b0e9289138f3d311
Kristen Colavita <kristen@Kristens-MacBook-Pro.local>
2022-04-27 19:39:14 -0400

    netlify link

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000' //'https://genuine-lily-26cdb4.netlify.app'
+  origin: 'https://genuine-lily-26cdb4.netlify.app'
 }));
 
 const sess = {
